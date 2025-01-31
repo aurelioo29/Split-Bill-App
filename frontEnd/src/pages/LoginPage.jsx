5db53910b5f49d4304e45259c48a6a577c537fec
aurelioo29 <aureliolucio5@gmail.com>
2025-01-31 22:01:37 +0700

    feat: :triangular_flag_on_post: Create LoginPage and RegisterPage with corresponding forms and layout

--- frontEnd/src/pages/LoginPage.jsx
+++ frontEnd/src/pages/LoginPage.jsx
@@ -1,17 +1,11 @@
-import Background from "../components/Background";
-import assets from "../data/assets";
+import AuthContainer from "../components/AuthContainer";
+import AuthForm from "../components/AuthForm";
 
 const LoginPage = () => {
   return (
-    <Background>
-      <div className="flex items-center justify-center h-screen ">
-        <div className="flex flex-row items-center bg-amber-500 p-10 rounded-lg">
-          <img src={assets.login[0]} alt="" width={800} />
-
-          <h1>Testing</h1>
-        </div>
-      </div>
-    </Background>
+    <AuthContainer>
+      <AuthForm formType="login" />
+    </AuthContainer>
   );
 };
 
